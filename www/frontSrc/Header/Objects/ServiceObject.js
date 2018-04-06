bbe90e0f18be15e650f89949f249936e3f247c68
Ridene <rridene@centreon.com>
2018-04-06 08:55:47 +0200

    add clock component

--- www/frontSrc/Header/Objects/ServiceObject.js
+++ www/frontSrc/Header/Objects/ServiceObject.js
@@ -66,6 +66,13 @@ const styles = theme => ({
     },
     backgroundColor: theme.palette.unknown.main,
   },
+  chip: {
+    height: '8px',
+    width: '8px',
+    borderRadius: 20,
+    display: 'inline-table',
+    marginRight: 6,
+  },
   icon: {
     width: 34,
     height: 34,
