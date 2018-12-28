3c420042460e398a305dc87c2e82d6126b9797f2
Danijel Chalupka <danijel@simple-task.com>
2018-12-28 19:18:41 +0100

    Dynamic loader with bam check added

--- www/front_src/src/components/header/index.js
+++ www/front_src/src/components/header/index.js
@@ -8,13 +8,18 @@ import UserMenu from "../userMenu";
 import HostMenu from "../hostMenu";
 import ServiceStatusMenu from "../serviceStatusMenu";
 
-import {DynamicComponentPosition} from '@centreon/react-components';
+import {DynamicComponentPosition, DynamicComponentLoader} from '@centreon/react-components';
 
 import axios from '../../axios';
 
 class TopHeader extends Component {
 
+  state = {
+    DynamicLoader:React.Fragment
+  }
+
   refreshIntervalsApi = axios("internal.php?object=centreon_topcounter&action=refreshIntervals");
+  bamCheckApi = axios("internal.php?object=centreon_modules_webservice&action=getBamModuleInfo");
 
   getRefreshIntervals = () => {
     const { setRefreshIntervals } = this.props;
@@ -28,11 +33,25 @@ class TopHeader extends Component {
       });
   }
 
+  checkIfBamExist = () => {
+    this.bamCheckApi.post().then(response => {
+      if(response.data){
+        if(response.data.enabled){
+          this.setState({
+            DynamicLoader:DynamicComponentLoader
+          });
+        }
+      }
+    });
+  }
+
   UNSAFE_componentWillMount = () => {
+    this.checkIfBamExist()
     this.getRefreshIntervals()
   }
 
   render() {
+    const {DynamicLoader} = this.state;
     return (
       <header class="header">
         <div class="header-icons">
@@ -40,7 +59,8 @@ class TopHeader extends Component {
             <PollerMenu />
           </div>
           <div class="wrap wrap-right">
-            <DynamicComponentPosition componentName={'topCounterLeft'} />
+            <DynamicLoader componentName={'topCounter'} componentUrl={'./modules/centreon-bam-server/react/compiled-components/topCounter/index.html'}/>
+            <DynamicComponentPosition componentName={'topCounter'} />
             <HostMenu />
             <ServiceStatusMenu />
             <UserMenu />
